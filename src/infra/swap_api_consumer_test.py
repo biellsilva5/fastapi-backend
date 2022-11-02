998c478ffafdd6031dd90353a3979a57122c3281
Lucas Silva <lucas.silva@pmweb.com.br>
2022-11-02 18:06:05 -0300

    refactor: request and testing

--- src/infra/swap_api_consumer_test.py
+++ src/infra/swap_api_consumer_test.py
@@ -1,10 +1,19 @@
 from .swapi_api_consumer import SwapiApiConsumer
 
 
-def test_get_starships(requests_mock):
+def test_get_starships():
     ''' Testing get staships method'''
-    requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json={'some': 'thing'})
+    #requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json={'some': 'thing'})
 
-    response = SwapiApiConsumer().get_starships(1)
-    print(response)
+    swapi_api_consumer = SwapiApiConsumer()
+    page = 1
+
+    get_starships_response = swapi_api_consumer.get_starships(page=page)
+    
+    assert get_starships_response.request.method == 'GET'
+    assert get_starships_response.request.url == 'https://swapi.dev/api/starships/'
+    assert get_starships_response.request.params == {"page": page}
+
+    assert get_starships_response.status_code == 200
+    assert isinstance(get_starships_response.response["results"], list)
     
--- src/infra/swapi_api_consumer.py
+++ src/infra/swapi_api_consumer.py
@@ -1,16 +1,42 @@
+from typing import Type
 import requests
 import urllib3
+from requests import Request
+from collections import namedtuple
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class SwapiApiConsumer:
     '''
         consumer api for swapi
     '''
-    @classmethod
-    def get_starships(cls, page: int) -> any:
-        ''' get startships '''
-        params = {'page': page}
-        response = requests.get(
-            'https://swapi.dev/api/starships/', params=params,timeout=10, verify=False)
+    def __init__(self) -> None:
+        self.get_starships_response = namedtuple('GET_Starships', 'status_code request response')
 
-        return response.json()
+    def get_starships(self, page: int) -> any:
+        ''' get startships '''
+        req = requests.Request(
+            method='GET',
+            url='https://swapi.dev/api/starships/',
+            params={"page": page}
+        )
+        req_prepared = req.prepare()
+
+        response = self.__send_http_request(req_prepared)
+        return self.get_starships_response(
+            status_code=response.status_code, request=req, response=response.json()
+        )
+
+    @classmethod
+    def __send_http_request(cls, req_prepared: Type[Request]) -> any:
+        '''
+            Prepare a session and send http request
+            :param - req_prepared: Request Object with all params
+            :response - Http response raw
+        '''
+        http_session = requests.Session()
+        response = http_session.send(
+                        req_prepared,
+                        verify=False
+                    )
+        return response
